5c1c7f87acded34419393ec8563c4801a8ed4192
Serzh-Shpalyutin <serzh.shpalyutin@mail.ru>
2020-09-29 22:46:39 +0300

    added section products

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,15 @@
 $(function () {
     
-    $('.').slick({
-     
+    $('.slider__inner').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        appendArrows: $('.main__title--slider'),
+        // prevArrow: '<button id="prev" type="button" class="btn btn-juliet"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>',
+        // nextArrow: '<button id="next" type="button" class="btn btn-juliet"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>'
+    });
+
+    $(function () {
+        var mixer = mixitup('.products__box'); 
     });
 
 });
