5aab4e854158430a94c7f8870c8526ad516655d7
Serzh-Shpalyutin <serzh.shpalyutin@mail.ru>
2020-09-30 13:04:43 +0300

    added drop menu

--- app/js/main.js
+++ app/js/main.js
@@ -8,8 +8,13 @@ $(function () {
         // nextArrow: '<button id="next" type="button" class="btn btn-juliet"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>'
     });
 
+    $('.filter__menu-drop').on('click', function(){
+        $('.drop-down__list').slideToggle();
+    });
+
     $(function () {
         var mixer = mixitup('.products__box'); 
     });
+
 
 });
