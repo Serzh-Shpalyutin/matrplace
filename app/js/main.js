5c7a7ffcb707db0929cbe12490c4407e1afec612
Serzh-Shpalyutin <serzh.shpalyutin@mail.ru>
2020-09-30 14:50:18 +0300

    added products hover

--- app/js/main.js
+++ app/js/main.js
@@ -4,8 +4,6 @@ $(function () {
         autoplay: true,
         autoplaySpeed: 2000,
         appendArrows: $('.main__title--slider'),
-        // prevArrow: '<button id="prev" type="button" class="btn btn-juliet"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>',
-        // nextArrow: '<button id="next" type="button" class="btn btn-juliet"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>'
     });
 
     $('.filter__menu-drop').on('click', function(){
