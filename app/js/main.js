e1f7259be378dc2ead7701a2de46527158830b6a
Serzh-Shpalyutin <serzh.shpalyutin@mail.ru>
2020-10-15 04:57:03 +0300

    fix sections

--- app/js/main.js
+++ app/js/main.js
@@ -1,36 +1,38 @@
 $(function () {
-    
+
     $('.weekly__inner').slick({
         // autoplay: true,
         // autoplaySpeed: 2000,
-        appendArrows: $('.title--slider'),
+        arrows: true,
+        prevArrow: "<img src='../images/icons/prev.png' class='.weekly__prev' alt='prewies'>",
+        nextArrow: "<img src='../images/icons/next.png' class='.weekly__next' alt='next'>"
     });
 
-    $('.filter__menu-drop').on('click', function(){
+    $('.filter__menu-drop').on('click', function () {
         $('.drop-down').slideToggle();
     });
 
     $(function () {
-        var mixer = mixitup('.products__box'); 
+        var mixer = mixitup('.products__box');
     });
 
     $(".rate-star").rateYo({
         rating: 4,
         starWidth: "12px",
         readOnly: true,
-        spacing   : "5px"
-      });
+        spacing: "5px"
+    });
 
-      $('.followers__items').slick({
+    $('.followers__items').slick({
         // infinite: true,
         slidesToShow: 3,
         slidesToScroll: 1,
         arrows: false
-      });
+    });
 
-      $('.feedback__items').slick({
-          slidesToShow: 2,
-          slidesToScroll: 2,
+    $('.feedback__items').slick({
+        slidesToShow: 2,
+        slidesToScroll: 2,
         // autoplay: true,
         // autoplaySpeed: 2000,
         arrows: false,
